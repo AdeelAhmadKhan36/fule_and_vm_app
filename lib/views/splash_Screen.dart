c2c87c99b734c9be76b2e5e277d8b1602110d0e6
mtahamunir <112475114+mtahamunir@users.noreply.github.com>
2024-06-03 12:45:49 +0500

    theme and splash screen added

---
+++ lib/views/splash_Screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:fule_and_vm_app/const.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            color: Maincolor,
+          )
+        ],
+      ),
+    );
+  }
+}
